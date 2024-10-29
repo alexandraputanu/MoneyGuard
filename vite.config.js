8af8ec7f9d2abd8035cbd785ce93c73660cbc8f0
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-10-29 17:33:48 +0200

    4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/MoneyGuard/', // Înlocuiește cu numele repo-ului tău GitHub
+    base: '/MoneyGuard/', // adaugă această linie
     plugins: [react()],
+    build: {
+        sourcemap: true,
+    },
 });
